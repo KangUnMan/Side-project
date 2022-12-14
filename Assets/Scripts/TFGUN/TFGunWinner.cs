a851a12c9a5d965bd623ad7f982498e9041e246b
Hwan47 <103931325+Hwan47@users.noreply.github.com>
2022-12-14 13:30:38 +0900

    스크립트 수정

--- Assets/Scripts/TFGUN/TFGunWinner.cs
+++ Assets/Scripts/TFGUN/TFGunWinner.cs
@@ -23,15 +23,11 @@ public class TFGunWinner : MonoBehaviour
 
     private void Start()
     {
-      
+        StartCoroutine(PlayerCountStart());
     }
     void Update()
     {
-        if (CountOn != true)
-        {
-            CountOn = true;
-            StartCoroutine(PlayerCountStart());
-        }
+
        if(PlaysCount==0&&PhotonNetwork.IsMasterClient&&count==0)
         {
             TFGameRoundEndGame();
@@ -59,7 +55,7 @@ public class TFGunWinner : MonoBehaviour
 
     IEnumerator PlayerCountStart()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         PlaysCount = PhotonNetwork.CountOfPlayers; //인원수 설정
         Debug.Log(PlaysCount);
     }
